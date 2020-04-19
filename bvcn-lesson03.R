e3b280911647659ba7d4612860b3f5072e055929
sarahhu <sarahhu@sarahhus-mbp.lan>
2020-04-19 13:07:37 -0400

    lesson 3 revised

--- bvcn-lesson03.R
+++ bvcn-lesson03.R
@@ -3,36 +3,25 @@ gene_data <- read.delim("test-data-skoog.txt")
 # ?read.delim()
 head(gene_data)
 # Data are representative of taxa as rows and columns showing genes (values equal copies of those genes for each taxa). These 4 genes are required for the full pathway. We will show some examples of how to subset, filter, and perform basic calculations.
- 
 
-# Data frame versus matrix in R
-## Data frames can include different types of data
-class(gene_data)
-head(gene_data)
-str(gene_data) # Factors and integers included in this data
-
-
-# So matrices...
-# When do you use one over the other? It depends on what kind of data you're working with. If you're working with different types of information in the same table, you likely need a data frame. But if you're working with a single data type, a matrix may be better for you. Additionally, matrices are more efficient with respect to memory. Therefore, a lot of statistical tools/methods require a matrix as input. We will review this. #Compare str() results
-gene_mat <- as.matrix(gene_data)
-# str(gene_mat)
-# str(gene_data)
-# View(gene_mat)
-
-# Let's work with the data frame version of our test data
-# Colnames and rownames:
+# Colnames and rownames, "index" what appears in the columns and rows. It is like the bar A-Z at the top and the numbers on the left side bar of an Excel table.
 colnames(gene_data) # reports all of my column names! 
 
 # Let's change some of these because there is a typo
-colnames(gene_data)[6] <- "GENE_D" #Changes the 5th column name to "GENE_D"
-colnames(gene_data)[4:6] <- c("GENE_C", "GENE_B", "GENE_D") #change column headers for rows 4 through 6
-colnames(gene_data)
+colnames(gene_data)[6] <- "GENE_D" #Changes the 6th column name to "GENE_D"
+# Change columns headers for columns 4-6
+# colnames(gene_data)[4:6] <- c("GENE_C", "GENE_B", "GENE_D")
+
+colnames(gene_data) # Check output
 
 # Example of how to change the order (2 ways) - Which way would be more reproducible?
 colnames(gene_data)
+# Option 1
 gene_data_reorder <- gene_data[, c("Taxon", "MAG","GENE_A", "GENE_B", "GENE_C", "GENE_D")]
 
+# Option 2
 tmp <- gene_data[c(1:3,5,4,6)] #Combine specific columns "c()"
+
 
 
 # How about rows - these are just random numbers! R assigns increasing numbers automatically as row names, unless you specify. 
@@ -50,60 +39,117 @@ row.names(tmp) <- tmp$MAG
 # row.names(tmp) <- paste(tmp$Taxon, tmp$MAG, sep=" ")
 
 
-# Let's work on modifying the data frame and do some math!
-library(reshape2)
-library(tidyverse)
+# Let's work on modifying the data frame and do some math with dplyr.
+# library(reshape2)
+library(tidyverse) # plyr # dplyr
 # head(gene_data[1:2,])
 
-# Use wide-format data and subset to find taxa with all genes required in pathway
-# Subset taxa so they have both Gene C and D, but have either Gene A or B. This demonstrates that the taxa are capable of the pathway.
-
-# Let's try this one at a time:
-tmp <- subset(gene_data, (GENE_C > 0 & GENE_D > 0))
-
-tmp2 <- subset(gene_data, !(GENE_A == 0 ))
-
-# What about subsetting dataframe so all taxa have all genes in pathway? How would we do this?
+# Common base R commands to check out help menus for:
+# ?subset()
+# ?apply() # Also tapply(), sapply(), and lapply()
+# ?aggregate()
+# ?split()
 
 
+# Let's subset our data with subset()
+# I only want MAGs (rows or entries) where Gene A is great that zero
+head(gene_data[1:3,])
+tmp1 <- subset(gene_data, !(GENE_A == 0 ))
+tmp1 <- subset(gene_data, GENE_A > 0)
+
+## Other approaches
+# tmp1 <- subset(gene_data, (GENE_C > 0 & GENE_D > 0))
+# tmp1 <- subset(gene_data, MAG %in% "MAG_2")
+
+# Let's repeat this with tidyverse
+# ?filter()
+tmp2 <- filter(gene_data, GENE_A > 0)
+
+# What if I want to filter a set of MAGs of interest?
+mags_i_want <- c("MAG_4", "MAG_12", "MAG_24", "MAG_18")
+
+tmp3 <- filter(gene_data, MAG %in% mags_i_want)
+
+# How would we combine the statements above?
 
 
-# This data is in wide format. Convert to long format - this is more versatile.
+# Pipe operator - the key to tidy syntax!
+gene_data %>% filter(MAG %in% mags_i_want)
+head(gene_data)
+gene_data %>% head()
+
+
+# Filter to the mags I want, but only show the taxonomic identities. Also set it equal to a new data frame.
+gene_data %>% 
+  filter(MAG %in% mags_i_want) %>%
+  select(Taxon, MAG)
+
+
+
+# Repeat in base R
+subset(gene_data, MAG %in% mags_i_want)[,c("Taxon", "MAG")]
+
+
+# This data is in wide format. Convert to long format - this is more versatile in R.
 ## Compare long vs. wide format
-gene_data_long <- melt(gene_data_reorder)
-head(gene_data_long)
+# ?pivot_longer()
+# ?pivot_wider()
+colnames(gene_data)
+gene_data_long <- gene_data %>%
+  pivot_longer(GENE_A:GENE_D, names_to = "GENE", values_to = "COUNT")
+
+
 # View(gene_data)
 # View(gene_data_long)
 
 
 # Split Taxon column
-gene_data_long <- separate(gene_data_long, Taxon, c("phylum", "class", "order", "family", "genus", "species"), sep = ";", remove = FALSE)
-head(gene_data_long)
+gene_data_long_cols <- gene_data_long %>%
+  separate(Taxon, c("phylum", "class", "order", "family", "genus", "species"), sep = ";", remove = FALSE)
+head(gene_data_long_cols)
 
 
-#
-# Review summarise(), mutate(), & dplyr syntax
-#
-# Mean at Class level - across whole dataset (add Min and Max value to demonstrate syntax)
-mean_class <- gene_data_long %>%
-  group_by(class, variable) %>%
-  summarise(MEAN = mean(value)) %>%
+#Lets combine the two operations from above.
+gene_data_long <- gene_data %>%
+  pivot_longer(GENE_A:GENE_D, names_to = "GENE", values_to = "COUNT") %>%
+  separate(Taxon, c("phylum", "class", "order", "family", "genus", "species"), sep = ";", remove = FALSE) %>%
   data.frame
-head(mean_class)
+# head(gene_data_long)
+
+# Mean at Class level - across whole dataset (add Min and Max value to demonstrate syntax)
+# ?summarise()
+mean_class <- gene_data_long %>%
+  group_by(class, GENE) %>%
+  summarise(MEAN = mean(COUNT)) %>%
+  data.frame
+# head(mean_class)
 
 
 # Relative abundance of genes by Taxon
+# ?mutate()
 relabun_all <- gene_data_long %>%
-  group_by(class, variable) %>%
-  mutate(RELABUN = (value/sum(value))) %>%
+  group_by(class, GENE) %>%
+  mutate(RELABUN = (COUNT/sum(COUNT))) %>%
   data.frame
 head(relabun_all)
 
 ## Check relative abundance calculation
-# alpha_test <- subset(relabun_all, class %in% "c__Alphaproteobacteria")
+# alpha_test <- filter(relabun_all, class %in% "c__Alphaproteobacteria")
 # View(alpha_test)
 # sum(alpha_test$RELABUN)
 
+
+# Add additional calculations to the above summarise and mutate functions.
+# ?n()
+# ?n_distinct()
+# ?median()
+# ?max()
+# ?min()
+# ?var()
+# ?sd()
+
+
+# IF we have time to cover:
 
 # Average across replicates
 ## Let's simulate replicates that we need to average across
@@ -126,6 +172,10 @@ head(mean_class)
 
 
 
+# Addressing questions from R slack Channel
+
+# Change to binary
+# gene_data_long$PresenceAbsence<-ifelse(gene_data_long$value > 0, 1, 0)
 #
 # Review from last week - explaination:
 # iris[order(iris$Species, -iris$Petal.Length),]
